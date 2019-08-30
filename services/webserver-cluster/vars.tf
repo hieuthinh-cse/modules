7c355f68bc0c3db2d0fcabe0a5e2025dc86789b8
Theodore-Thinh Nguyen <legolas.vnz@gmail.com>
2019-08-30 13:34:40 +0000

    0.11 version

--- services/webserver-cluster/vars.tf
+++ services/webserver-cluster/vars.tf
@@ -4,33 +4,33 @@
 # # ---------------------------------------------------------------------------------------------------------------------
 
 variable "cluster_name" {
-	description="The name to use for all the cluster resources"
-	type = string
+  description = "The name to use for all the cluster resources"
+  type        = "string"
 }
 
 variable "db_remote_state_bucket" {
-	description="The name of the S3 bucket for the database's remote state"
-	type = string
+  description = "The name of the S3 bucket for the database's remote state"
+  type        = "string"
 }
 
 variable "db_remote_state_key" {
-	description="The path for the database's remote state in S3"
-	type = string 
+  description = "The path for the database's remote state in S3"
+  type        = "string"
 }
 
 variable "instance_type" {
-	description="The type of EC2 Instances to run (e.g t2.micro)"
-	type = string
+  description = "The type of EC2 Instances to run (e.g t2.micro)"
+  type        = "string"
 }
 
 variable "min_size" {
-	description = "The minimum number of EC2 Instance in the ASG"
-	type = number
+  description = "The minimum number of EC2 Instance in the ASG"
+  type        = "string"
 }
 
 variable "max_size" {
-	description = "The maximum number of EC2 Instance in the ASG"
-	type = number
+  description = "The maximum number of EC2 Instance in the ASG"
+  type        = "string"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -41,6 +41,6 @@ variable "max_size" {
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
+  type        = "string"
+  default     = "8080"
 }
